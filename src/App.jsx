1db9002504a9d06b40075e5521a30f9fd3539c44
b-4321 <bishwadeepkhanra4321@gmail.com>
2025-04-04 12:28:45 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,65 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Background from './Components/Background'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import About from './Components/About'
+import Explore from './Components/Explore'
+import Contact from './Components/Contact'
+import Rent from './Components/Rent'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+
+    },
+    {
+      path: "/about",
+      element: <About/>,
+    },
+    {
+      path: "/explore",
+      element: <Explore/>,
+    },
+    {
+      path: "/contact",
+      element: <Contact/>,
+    },
+    {
+      path: "/rent",
+      element: <Rent/>,
+    },
+  ])
 
   return (
+    
     <>
-      <h1 className='text-red-700 text-4xl text-center'> Let's get code guys </h1>
+    
+      <div>
+        <Navbar/>
+      </div>
+        <div className='h-screen  relative overflow-hidden my-1 mx-1 rounded-2xl'>
+        <Background/>
+         </div>
+
+         <div className=' bg-slate-500 p-36 justify-around rounded-lg mx-1 my-1 flex'>
+          
+         </div>
+
+         <div className='p-60 bg-teal-600 items-center justify-center rounded-lg mx-1 my-1'>
+          hello
+         </div>
+
+         <footer className='bg-gray-800 text-white justify-center rounded-lg mx-1 p-14 my-1'>
+            fOOTER
+         </footer>
+         
     </>
+    
   )
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
+import { IoCarSportOutline } from "react-icons/io5";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+  return (
+    <>
+        <nav className='rounded-lg overflow-hidden shadow-lg bg-gray-900 my-1 mx-1'>
+            <div className='flex justify-between items-center p-4 bg-gray-800 text-white'>
+                <div className='flex items-center'>
+                    <IoCarSportOutline className='text-3xl' />
+                    <span className='ml-2 text-xl font-bold'>Car Hunt</span>
+                </div>
+                <div className='flex space-x-10'>
+                    <a href="/" className='hover:text-gray-400'>Home</a>
+                    <a href="/rent" className='hover:text-gray-400'>Rent</a>
+                    <a href="/explore" className='hover:text-gray-400'>Explore   </a>
+                    <a href="/about" className='hover:text-gray-400'>About Us</a>
+                    <a href="/contact" className='hover:text-gray-400'>Contact</a>
+                </div>
+                <div className='flex items-center space-x-10'>
+                    <FaSearch className='text-xl' />
+                    <FaShoppingCart className='text-xl' />
+                    <button className='bg-blue-500 text-white flex px-4 py-2 rounded hover:bg-red-600 backdrop-blur-md animate-bounce'>
+                        Login
+                        <FaArrowRight className='ml-2 my-auto' />
+                    </button>
+                </div>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
