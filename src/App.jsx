c95d8b8e0ec91e5f1d388e170b5ed5349823f270
b-4321 <bishwadeepkhanra4321@gmail.com>
2025-04-07 21:21:53 +0530

    Home component modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Background from './Components/Background';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import About from './Components/About';
@@ -9,40 +8,19 @@ import Explore from './Components/Explore';
 import Contact from './Components/Contact';
 import Rent from './Components/Rent';
 import Login from './Components/login';
-import Footer from './Components/Footer';
-import Carousel from './Components/Carousel';
+
+// Define the routes
+const router = createBrowserRouter([
+  { path: "/", element: <><Navbar /><Home /></> },
+  { path: "/about", element: <><Navbar /><About /></> },
+  { path: "/explore", element: <><Navbar /><Explore /></> },
+  { path: "/contact", element: <><Navbar /><Contact /></> },
+  { path: "/rent", element: <><Navbar /><Rent /></> },
+  { path: "/login", element: <><Navbar /><Login /></> },
+]);
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-      </div>
-      <div className='h-screen relative overflow-hidden my-1 mx-1 rounded-2xl'>
-        <Background />
-      </div>
-      <div className='bg-slate-500 px-10 py-24 rounded-2xl mx-1 my-1'>
-        <Carousel />
-      </div>
-      <div className='p-60 bg-teal-600 items-center justify-center rounded-2xl mx-1 my-1'>
-        hello
-      </div>
-      <footer className='bg-gray-800 text-white justify-center rounded-2xl mx-1 p-14 my-1'>
-        <Footer />
-      </footer>
-
-      {/* Routes for the application */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/rent" element={<Rent />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
