c33534895e69578cbdec5363a32852f5225d93d9
b-4321 <bishwadeepkhanra4321@gmail.com>
2025-04-07 15:34:48 +0530

    slider modified

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ import Explore from './Components/Explore'
 import Contact from './Components/Contact'
 import Rent from './Components/Rent'
 import Login from './Components/login'
+import Footer from './Components/Footer'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Carousel from './Components/Carousel'
 
 function App() {
   const router = createBrowserRouter([
@@ -52,16 +54,16 @@ function App() {
         <Background/>
          </div>
 
-         <div className=' bg-slate-500 p-36 justify-around rounded-lg mx-1 my-1 flex'>
-          
+         <div className=' bg-slate-500 px-10 py-24  rounded-2xl mx-1 my-1'>
+          <Carousel/>
          </div>
 
-         <div className='p-60 bg-teal-600 items-center justify-center rounded-lg mx-1 my-1'>
+         <div className='p-60 bg-teal-600 items-center justify-center rounded-2xl mx-1 my-1'>
           hello
          </div>
 
-         <footer className='bg-gray-800 text-white justify-center rounded-lg mx-1 p-14 my-1'>
-            fOOTER
+         <footer className='bg-gray-800 text-white justify-center rounded-2xl mx-1 p-14 my-1'>
+            <Footer/>
          </footer>
          
     </>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,12 +26,12 @@ const Navbar = () => {
                 <div className='flex items-center space-x-10'>
                     <FaSearch className='text-xl' />
                     <FaShoppingCart className='text-xl' />
-                    <Link to="/Login">
+                    <a href="/Login">
                         <button className='bg-blue-500 text-white flex px-4 py-2 rounded hover:bg-red-600 backdrop-blur-md animate-bounce'>
                             Login
                             <FaArrowRight className='ml-2 my-auto' />
                         </button>
-                    </Link>
+                    </a>
                 </div>
             </div>
         </nav>
