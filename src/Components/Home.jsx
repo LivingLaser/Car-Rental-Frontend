56dc1200f9679aebf011048047184ab78b6953fa
b-4321 <bishwadeepkhanra4321@gmail.com>
2025-06-03 16:12:16 +0530

    home theme changed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -19,14 +19,14 @@ function Home() {
       <div className='h-screen relative overflow-hidden my-1 mx-1 rounded-2xl'>
         <Background />
       </div>
-      <div className='bg-slate-500 px-10 py-24 rounded-2xl mx-1 my-1'>
+      <div className='bg-slate-600 px-10 py-24 rounded-2xl mx-1 my-1 glass-effect'>
         <Carousel />
       </div>
-      <div className='p-10 bg-cyan-400 items-center justify-center rounded-2xl mx-1 my-1'>
-        <div className=' my-2 mx-0 text-center text-xl font-bold border-4 border-pink-500 rounded-2xl py-2'>Search Your Nearby Cars............</div>
+      <div className='p-10 bg-gray-500 items-center justify-center rounded-2xl mx-1 my-1 glass-effect'>
+        <div className=' my-2 mx-0 text-center text-xl font-bold border-4 border-pink-500 rounded-2xl py-2 '>Search Your Nearby Cars............</div>
         <Maps/>
       </div>
-      <div className='p-32 bg-purple-800 items-center justify-center rounded-2xl mx-1 my-1'>
+      <div className='p-32 bg-neutral-500 items-center justify-center rounded-2xl mx-1 my-1 glass-effect'>
         <Rating/>
       </div>
       <footer className='bg-gray-800 text-white justify-center rounded-2xl mx-1 p-14 my-1'>
