af561e7fa24a8675bb07195963d6a6da1a29aca3
LivingLaser <dassayantan450@gmail.com>
2025-06-12 18:49:15 +0530

    minor changes;

--- src/Components/Rent2.jsx
+++ src/Components/Rent2.jsx
@@ -1,55 +1,65 @@
 import { TextField } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { CAR_IMAGE_RESOURCE } from '../services/carService';
 import { BsFillFuelPumpFill } from "react-icons/bs";
 import { IoSpeedometer } from "react-icons/io5";
 import { MdOutlineAirlineSeatReclineExtra } from "react-icons/md";
 import { BsSuitcase } from "react-icons/bs";
-
+import { toast } from 'react-toastify';
+import { userBooking } from '../services/bookingService';
+import userContext from '../auth/userContext';
 
 const Rent = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const car = location.state?.car;
+  const userData = useContext(userContext);
 
-  
-  const [pickupDate, setPickupDate] = useState('');
-  const [dropoffDate, setDropoffDate] = useState('');
-  const [totalPrice, setTotalPrice] = useState(null);
-  const [pickupLocation, setPickupLocation] = useState('');
-  const [dropoffLocation, setDropoffLocation] = useState('');
+  const [bookingDetail, setBookingDetail] = useState({
+    pickLocation: "",
+    dropLocation: "",
+    pickDate: "",
+    dropDate: "",
+    amount: ""
+  });
 
- 
-
+  const [errors, setErrors] = useState({});
   const hourlyPrice = (car.car.rentPrice);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleChange = (event) => {
+    const {id, value} = event.target;
+    setBookingDetail({ ...bookingDetail, [id]: value });
+  }
 
-    if (!pickupLocation || !dropoffLocation) {
-      alert('Please enter both pickup and drop-off locations.');
-      return;
-    }
-
-    if (!pickupDate || !dropoffDate) {
-      alert('Please select both pickup and drop-off dates.');
-      return;
-    }
-
-    const pickup = new Date(pickupDate);
-    const dropoff = new Date(dropoffDate);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const pickup = new Date(bookingDetail.pickDate);
+    const dropoff = new Date(bookingDetail.dropDate);
 
     if (pickup >= dropoff) {
-      alert('Drop-off date must be later than pickup date.');
+      toast.error("Drop-off date must be later than Pickup date");
       return;
     }
 
     const durationInHours = Math.ceil((dropoff - pickup) / (1000 * 60 * 60));
-    const calculatedTotalPrice = (durationInHours * hourlyPrice).toFixed(2);
+    const totalPrice = (durationInHours * hourlyPrice).toFixed(2);
+    setBookingDetail({ ...bookingDetail, [amount]: totalPrice });
 
-    setTotalPrice(calculatedTotalPrice);
-    alert(`Booking confirmed! Total price: ₹${calculatedTotalPrice}`);
+    userBooking(bookingDetail, userData.user.userId, car.car.modelId).then((response) => {
+      setBookingDetail({
+        pickLocation: "",
+        dropLocation: "",
+        pickDate: "",
+        dropDate: "",
+        amount: ""
+      });
+      setErrors({});
+      toast.success("Booking initiated...");
+      navigate("/booking");
+    }).catch((error) => {
+      setErrors(error);
+    });
   };
 
   return (
@@ -62,22 +72,22 @@ const Rent = () => {
         <img src={CAR_IMAGE_RESOURCE + car.car.modelImage} alt={car.car.modelName} className="w-full h-70 object-cover rounded-md mb-4" />
         <h2 className="text-2xl font-bold text-center mb-2">Model : {car.car.modelName}</h2>
         <div className="flex justify-around gap-4 mb-6 items-center">
-                  <div className="flex items-center p-1">
-                    <BsFillFuelPumpFill className="text-2xl text-blue-500 mr-2" />
-                    <p className="text-black font-semibold">Fuel : {car.car.fuelType}</p>
-                  </div>
-                  <div className="flex items-center p-1">
-                    <IoSpeedometer className="text-2xl text-blue-500 mr-2" />
-                    <p className="text-black font-semibold">Mileage : {car.car.mileage}</p>
-                  </div>
-                  <div className="flex items-center p-1">
-                    <MdOutlineAirlineSeatReclineExtra className="text-2xl text-blue-500 mr-2" />
-                    <p className="text-black font-semibold">Seat Capacity : {car.car.seatCapacity}</p>
-                  </div>
-                  <div className="flex items-center p-1">
-                    <BsSuitcase className="text-2xl text-blue-500 mr-2" />
-                    <p className="text-black font-semibold">Boot Space : {car.car.bootSpace}</p>
-                  </div>
+          <div className="flex items-center p-1">
+            <BsFillFuelPumpFill className="text-2xl text-blue-500 mr-2" />
+            <p className="text-black font-semibold">Fuel : {car.car.fuelType}</p>
+          </div>
+          <div className="flex items-center p-1">
+            <IoSpeedometer className="text-2xl text-blue-500 mr-2" />
+            <p className="text-black font-semibold">Mileage : {car.car.mileage}</p>
+          </div>
+          <div className="flex items-center p-1">
+            <MdOutlineAirlineSeatReclineExtra className="text-2xl text-blue-500 mr-2" />
+            <p className="text-black font-semibold">Seat Capacity : {car.car.seatCapacity}</p>
+          </div>
+          <div className="flex items-center p-1">
+            <BsSuitcase className="text-2xl text-blue-500 mr-2" />
+            <p className="text-black font-semibold">Boot Space : {car.car.bootSpace}</p>
+          </div>
         </div>
         <p className="text-lg font-semibold text-center mb-4">Hourly-Price: ₹{hourlyPrice}/hour</p>
         <form onSubmit={handleSubmit}>
@@ -85,37 +95,29 @@ const Rent = () => {
             <div>
               <label className="block text-sm font-medium mb-1">Pickup Location</label>
               <TextField
-                type="text"
-                className="w-full p-2 border rounded-md"
-                value={pickupLocation}
-                onChange={(e) => setPickupLocation(e.target.value)}
+                typ className="w-full p-2 border rounded-md"
+                id="pickLocation" value={bookingDetail.pickLocation} onChange={handleChange}
               />
             </div>
             <div>
               <label className="block text-sm font-medium mb-1">Drop-off Location</label>
               <TextField
-                type="text"
-                className="w-full p-2 border rounded-md"
-                value={dropoffLocation}
-                onChange={(e) => setDropoffLocation(e.target.value)}
+                type="text" className="w-full p-2 border rounded-md"
+                id="dropLocation" value={bookingDetail.dropLocation} onChange={handleChange}
               />
             </div>
             <div>
               <label className="block text-sm font-medium mb-1">Pickup Date</label>
               <TextField
-                type="datetime-local"
-                className="w-full p-2 border rounded-md"
-                value={pickupDate}
-                onChange={(e) => setPickupDate(e.target.value)}
+                type="datetime-local" className="w-full p-2 border rounded-md"
+                id="pickDate" value={bookingDetail.pickDate} onChange={handleChange}
               />
             </div>
             <div>
               <label className="block text-sm font-medium mb-1">Drop-off Date</label>
               <TextField
-                type="datetime-local"
-                className="w-full p-2 border rounded-md"
-                value={dropoffDate}
-                onChange={(e) => setDropoffDate(e.target.value)}
+                type="datetime-local" className="w-full p-2 border rounded-md"
+                id="dropDate" value={bookingDetail.dropDate} onChange={handleChange}
               />
             </div>
           </div>
